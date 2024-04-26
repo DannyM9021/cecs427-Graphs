ad515ded35b9212d773c4bef6981b11ec0e91ab4
DannyM9021 <dmoreno.1109@gmail.com>
2024-04-26 14:02:25 -0700

    Initializing file to parse incoming file and initialized a scrapy project

---
+++ crawl.py
@@ -0,0 +1,16 @@
+# Daniel Moreno
+# CECS 427 - 01 Dynamic Networks
+# Due Data: April 26, 2024
+
+# This program will parse the given document
+# Then send parameters to the scrapy spider to get the nodes we want
+
+# Parsing the file given
+def file_parse():
+    pass
+
+def main():
+    pass
+
+main()
+
