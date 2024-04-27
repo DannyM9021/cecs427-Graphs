94c1d59757e7f21080ef43d3735a04bcae95430b
DannyM9021 <dmoreno.1109@gmail.com>
2024-04-26 22:57:13 -0700

    Added a few comments and got rid of crawl.py file that is no longer needed

--- network/network/spiders/www_spider.py
+++ network/network/spiders/www_spider.py
@@ -26,22 +26,28 @@ class www_spider(scrapy.Spider):
         self.vertices = file_parse[0]
         self.dict = {}
 
+    # Main function that is called when the spider is started
     def parse(self, response):
         try:
+            # Saving the start url as key with the visited pages as a list serving as the key
             self.dict.update({response.url:response.css('span[itemprop="author"] a::attr(href)').getall()})
+            # Outputting to make sure that the spider is crawling correctly
             for links in response.css('span[itemprop="author"] a::attr(href)').getall():
                 print(links)
                 try:
                     yield{"link":links}
                 except Exception as e:
                     yield{"link":"NONE"}
+        # Exiting if there is a problem that is encountered
         except Exception as e:
             self.logger.error("Error parsing response: %s", e)
+    # Parsing the crawlFile that was given, don't know if a different file will be used
     def parse_input_file(self):
         curr_dir = os.path.dirname(os.path.abspath(__file__))
         file_path = os.path.join(curr_dir, '..', '..', '..', 'crawlingFile')
 
         items = []
+        # Obtaining the information by removing the newline symbol
         try:
             with open(file_path, 'r') as file:
                 for line in file:
@@ -49,8 +55,10 @@ class www_spider(scrapy.Spider):
                 file.close()
                 print(items)
                 return items
+        # If error, then exits with a message
         except Exception as e:
             print("Error:", e)
+            return []
 
     # When spider is done executing, this funciton will be called, will output the file
     # https://stackoverflow.com/questions/12394184/scrapy-call-a-function-when-a-spider-quits
